d0f0933adbafb4c88d1a14f71a95d11f08f8066b
kiandr <davoudi.rk@gmail.com>
2016-01-11 10:13:22 -0800

    Added the funtions

--- Tree.c
+++ Tree.c
@@ -1,1 +1,39 @@
 // This is the main file
+#include "stdio.h"
+
+typedef struct TreeStruct {
+    TreeStruct *left;
+    TreeStruct *right;
+    TreeStruct *par;
+    TreeStruct *current;
+    TreeStruct *next;
+} treeNode;
+
+
+static typedef struct TreeFunctions {
+    void Add(treeNode *root, int val);
+    void Print (treeNode *root);
+
+}funcs;
+
+// Implementation
+void Add(treeNode *root, int val){};
+
+void Print(treeNode *root){};
+
+
+
+int main(){
+    treeNode *root = (treeNode*)malloc(sizeof(treeNode));
+    root->left =0;
+    root->right = 0;
+    root->current = 5;
+    root->par = NULL;
+    root->next = NULL;
+    
+    
+    Add(root, 6);
+    Add(root,4);
+
+
+};
